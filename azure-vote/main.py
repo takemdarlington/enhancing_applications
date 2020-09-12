cb4c8bf456f67f009745a011966940fea4427ad6
takemdarlington <takemdarlington@gmail.com>
2020-09-12 02:36:42 +0100

    change logging to info

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -108,11 +108,11 @@ def index():
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
-            logger.info("Cat Vote", extra=properties)
+            logger.info("Cats Vote", extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            logger.warning('Dog vote', extra=properties)
+            logger.info('Dogs vote', extra=properties)
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
