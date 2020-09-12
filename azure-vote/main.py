a5367daf311164ce5e73e15cd920d9fd13db7cdd
takemdarlington <takemdarlington@gmail.com>
2020-09-12 10:37:56 +0100

    update main.py

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -28,22 +28,22 @@ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 logger = logging.getLogger(__name__)
 
 logger.addHandler(
-    AzureLogHandler(connection_string="InstrumentationKey=be659f5f-cf98-41be-8d25-2881254a5e60;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/")
+    AzureLogHandler(connection_string="InstrumentationKey=cfcc8494-c705-4dee-8bee-49d15b5df738;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/")
 )
 logger.addHandler(
-    AzureEventHandler(connection_string="InstrumentationKey=be659f5f-cf98-41be-8d25-2881254a5e60;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/")
+    AzureEventHandler(connection_string="InstrumentationKey=cfcc8494-c705-4dee-8bee-49d15b5df738;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/")
 )
 logger.setLevel(logging.INFO)
 
 # Metrics
 exporter = metrics_exporter.new_metrics_exporter(
     enable_standard_metrics=True,
-    connection_string="InstrumentationKey=be659f5f-cf98-41be-8d25-2881254a5e60;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/"
+    connection_string="InstrumentationKey=cfcc8494-c705-4dee-8bee-49d15b5df738;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/"
 )
 
 # Tracing
 tracer = Tracer(
-    exporter=AzureExporter(connection_string="InstrumentationKey=be659f5f-cf98-41be-8d25-2881254a5e60;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/"),
+    exporter=AzureExporter(connection_string="InstrumentationKey=cfcc8494-c705-4dee-8bee-49d15b5df738;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/"),
     sampler=ProbabilitySampler(1.0),
 )
 
@@ -52,7 +52,7 @@ app = Flask(__name__)
 # Requests
 middleware = FlaskMiddleware(
     app,
-    exporter=AzureExporter(connection_string="InstrumentationKey=be659f5f-cf98-41be-8d25-2881254a5e60;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/"),
+    exporter=AzureExporter(connection_string="InstrumentationKey=cfcc8494-c705-4dee-8bee-49d15b5df738;IngestionEndpoint=https://westus2-1.in.applicationinsights.azure.com/"),
     sampler=ProbabilitySampler(rate=1.0),
 )
 
